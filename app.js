025d92d50a9bd7b445d357fbb51d05d88c4ef0dc
unknown <pasanbegovicamer@gmail.com>
2023-01-23 16:31:18 +0100

    Project structure

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+require('express-async-errors'); //instead of using unnecessary try and catch blocks
+const express = require('express');
+const cors = require('cors')
+const app = express();
+//const userRouter = require('./routes/userRoute')
+
+//connectDB
+const connect = require('./db/connect')
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+// extra packages
+app.use(cors({
+  origin: "*",
+  methods: '*',
+  allowedHeaders: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}));
+
+// routes
+//app.use('/api/v1/users', authMiddleware, userRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connect(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
